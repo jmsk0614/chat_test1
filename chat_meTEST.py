651eb3afb0295bc80604ca156161ac8dc0180886
jmsk0614 <120787838+jmsk0614@users.noreply.github.com>
2024-12-16 20:59:38 +0900

    Update chat_meTEST.py

--- chat_meTEST.py
+++ chat_meTEST.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+import toml
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.messages import ChatMessage
 from langchain_core.prompts import PromptTemplate
@@ -7,13 +8,11 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import load_prompt
 
 
-from dotenv import (
-    load_dotenv,
-)  # python-dotenv 패키지를 사용하여 .env 파일에 저장된 환경 변수를 로드
+# secrets.toml에서 API 키 읽기
 
-# 환경변수 로드
-load_dotenv()  # .env 파일을 로드하여 환경 변수 설정
-openai_api_key = os.getenv("OPENAI_API_KEY")  # .env에 저장된 OPENAI_API_KEY를 가져옴
+secrets = toml.load("secrets.toml")
+
+open_api_key = secrets["DEFAULT"]["OPENAI_API_KEY"]
 
 
 st.set_page_config(page_title="나만의 ChatGPT 💬", page_icon="💬")
